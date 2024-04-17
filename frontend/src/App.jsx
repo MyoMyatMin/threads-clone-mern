3e4e3642ec4ff71982bdd60c438ff1849e9e7429
Myo Myat Min <myomyatmin315@gmail.com>
2024-04-17 23:59:15 +0700

    initilize ui for chatting and compete conversation header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from "@chakra-ui/react";
+import { Box, Button, Container } from "@chakra-ui/react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import UserPage from "../src/pages/UserPage";
 import PostPage from "../src/pages/PostPage";
@@ -7,6 +7,7 @@ import HomePage from "../src/pages/HomePage";
 import AuthPage from "../src/pages/AuthPage";
 import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
+import Chatpage from "./pages/Chatpage";
 
 import UpdateProfilePage from "./pages/UpdateProfilePage";
 import CreatePost from "./components/CreatePost";
@@ -15,39 +16,45 @@ function App() {
   const user = useRecoilValue(userAtom);
 
   return (
-    <Container maxW="620px">
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <HomePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={!user ? <AuthPage /> : <Navigate to="/" />}
-        />
+    <Box position={"relative"} w="full">
+      <Container maxW="620px">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!user ? <AuthPage /> : <Navigate to="/" />}
+          />
 
-        <Route
-          path="/update"
-          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/:username"
-          element={
-            user ? (
-              <>
+          <Route
+            path="/update"
+            element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/:username"
+            element={
+              user ? (
+                <>
+                  <UserPage />
+                  <CreatePost />
+                </>
+              ) : (
                 <UserPage />
-                <CreatePost />
-              </>
-            ) : (
-              <UserPage />
-            )
-          }
-        />
-        <Route path="/:username/post/:pid" element={<PostPage />} />
-      </Routes>
-      {/* {user && <LogoutButton />} */}
-    </Container>
+              )
+            }
+          />
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+          <Route
+            path="/chat"
+            element={user ? <Chatpage /> : <Navigate to={"/auth"} />}
+          />
+        </Routes>
+        {/* {user && <LogoutButton />} */}
+      </Container>
+    </Box>
   );
 }
 
---
+++ frontend/src/pages/Chatpage.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import Conversation from "../components/Conversation.jsx";
+
+const Chatpage = () => {
+  return (
+    <Box
+      position={"absolute"}
+      left={"50%"}
+      w={{ base: "100%", md: "80%", lg: "750px" }}
+      transform={"translateX(-50%)"}
+      p={4}
+      border={"1px solid red"}
+    >
+      <Flex
+        gap={4}
+        flexDirection={{
+          base: "column",
+          md: "row",
+        }}
+        maxW={{
+          sm: "400px",
+          md: "full",
+        }}
+        mx={"auto"}
+      >
+        <Flex
+          flex={30}
+          gap={2}
+          flexDirection={"column"}
+          maxW={{
+            sm: "250px",
+            md: "full",
+          }}
+        >
+          <Text
+            fontWeight={700}
+            color={useColorModeValue("gray.600", "gray.400")}
+          >
+            Your Conversations
+          </Text>
+          <form action="">
+            <Flex alignItems={"center"} gap={2}>
+              <Input placeholder="Search for a user" />
+              <Button size={"sm"}>
+                <SearchIcon />
+              </Button>
+            </Flex>
+          </form>
+          {true &&
+            [0, 1, 2, 3, 4, 5].map((_, i) => (
+              <Flex
+                key={i}
+                gap={4}
+                alignItems={"center"}
+                p={"1"}
+                borderRadius={"md"}
+              >
+                <Box>
+                  <SkeletonCircle size={10} />
+                </Box>
+                <Flex w={"full"} flexDirection={"column"} gap={3}>
+                  <Skeleton h={"10px"} w={"80px"} />
+                  <Skeleton h={"8px"} w={"90%"} />
+                </Flex>
+              </Flex>
+            ))}
+          <Conversation />
+        </Flex>
+        <Flex flex={70}>MessageContainer</Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Chatpage;
