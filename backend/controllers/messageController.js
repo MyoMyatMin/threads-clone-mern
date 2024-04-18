d6a5eb615d2156ff83a4b7eeef29b8a2f742f094
Myo Myat Min <myomyatmin315@gmail.com>
2024-04-18 23:45:14 +0700

    create message model, conversation model and complete sendMessage endpont

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,41 @@
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/messageModel.js";
+const sendMessage = async (req, res) => {
+  try {
+    const { recipentId, message } = req.body;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recipentId] },
+    });
+    if (!conversation) {
+      conversation = new Conversation({
+        participants: [senderId, recipentId],
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      });
+      await conversation.save();
+    }
+    const newMessage = new Message({
+      conversationId: conversation._id,
+      sender: senderId,
+      text: message,
+    });
+    await Promise.all([
+      newMessage.save(),
+      conversation.updateOne({
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      }),
+    ]);
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { sendMessage };
