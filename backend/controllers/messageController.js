cae89dda99f2c6663a6e8d61c73e47fff0bd23c6
Myo Myat Min <myomyatmin315@gmail.com>
2024-04-19 23:33:22 +0700

    complete getMessages and getConversations endpoints

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -38,4 +38,42 @@ const sendMessage = async (req, res) => {
   }
 };
 
-export { sendMessage };
+const getMessages = async (req, res) => {
+  const { otherUserId } = req.params;
+  const userId = req.user._id;
+  try {
+    const conversation = await Conversation.findOne({
+      participants: { $all: [userId, otherUserId] },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ error: "Coversation not found." });
+    }
+
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getConversations = async (req, res) => {
+  const userId = req.user._id;
+  try {
+    const conversations = await Conversation.find({
+      participants: userId,
+    }).populate({
+      path: "participants",
+      select: "username profilePic",
+    });
+
+    res.status(200).json(conversations);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { sendMessage, getMessages, getConversations };
